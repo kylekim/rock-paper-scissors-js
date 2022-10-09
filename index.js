887605dfc7aab923454ce1939b3fd7937e8dd281
Kyle Kim <kylekimrice@gmail.com>
2022-10-09 07:57:27 -0400

    Finished UI for RPS

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ function getComputerChoice() {
     }
 }
 
-function singleRound(playerSelection, computerSelection) {
+function singleRound(playerSelection) {
     let player = playerSelection.toLowerCase();
 
-    let computer = computerSelection.toLowerCase();
+    let computer = getComputerChoice().toLowerCase();
 
     player = player.charAt(0).toUpperCase() + player.slice(1);
 
@@ -55,13 +55,116 @@ function singleRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Enter your move: ");
-        const computerSelection = getComputerChoice();
-        console.log(singleRound(playerSelection, computerSelection));
-    }
-    
-}
+let playerScore = 0;
+let computerScore = 0;
 
-game();
+const button1 = document.createElement("button");
+button1.innerHTML = "Rock";
+document.body.appendChild(button1);
+button1.addEventListener("click", function x() {
+    if (playerScore !=5 && computerScore != 5) {
+        const resultText = singleRound("Rock");
+
+        if (resultText.slice(0, 8) == "You Win!") {
+            playerScore += 1;
+        } else if (resultText.slice(0, 9) == "You Lose!") {
+            computerScore += 1;
+        } 
+
+        const result = document.createElement("div");
+        result.textContent = resultText + ` [${playerScore} - ${computerScore}]`;
+
+        document.getElementById("results").appendChild(result);
+
+        if (playerScore == 5 && computerScore < 5) {
+            const finalResult = document.createElement("div");
+            finalResult.textContent = "Player Wins!";
+            document.getElementById("results").appendChild(finalResult);
+
+        } else if (computerScore == 5 && playerScore < 5) {
+            const finalResult = document.createElement("div");
+            finalResult.textContent = "Computer Wins!";
+            document.getElementById("results").appendChild(finalResult);
+        } else if (playerScore == 5 && computerScore == 5) {
+            const finalResult = document.createElement("div");
+            finalResult.textContent = "Player & Computer Tied!";
+            document.getElementById("results").appendChild(finalResult);
+        }
+    }
+});
+
+const button2 = document.createElement("button");
+button2.innerHTML = "Paper";
+document.body.appendChild(button2);
+button2.addEventListener("click", function x() {
+    if (playerScore !=5 && computerScore != 5) {
+        const resultText = singleRound("Paper");
+    
+        if (resultText.slice(0, 8) == "You Win!") {
+            playerScore += 1;
+        } else if (resultText.slice(0, 9) == "You Lose!") {
+            computerScore += 1;
+        } 
+        
+        const result = document.createElement("div");
+        result.textContent = resultText + ` [${playerScore} - ${computerScore}]`;
+        document.getElementById("results").appendChild(result);
+        
+        if (playerScore == 5 && computerScore < 5) {
+            const finalResult = document.createElement("div");
+            finalResult.textContent = "Player Wins!";
+                document.getElementById("results").appendChild(finalResult);
+        } else if (computerScore == 5 && playerScore < 5) {
+            const finalResult = document.createElement("div");
+            finalResult.textContent = "Computer Wins!";
+            document.getElementById("results").appendChild(finalResult);
+        } else if (playerScore == 5 && computerScore == 5) {
+            const finalResult = document.createElement("div");
+            finalResult.textContent = "Player & Computer Tied!";
+            document.getElementById("results").appendChild(finalResult);
+        }
+    }
+});
+
+
+const button3 = document.createElement("button");
+button3.innerHTML = "Scissors";
+document.body.appendChild(button3);
+button3.addEventListener("click", function x() {
+    if (playerScore !=5 && computerScore != 5) {
+        const resultText = singleRound("Scissors");
+
+        if (resultText.slice(0, 8) == "You Win!") {
+            playerScore += 1;
+        } else if (resultText.slice(0, 9) == "You Lose!") {
+            computerScore += 1;
+        } 
+    
+        const result = document.createElement("div");
+        result.textContent = resultText + ` [${playerScore} - ${computerScore}]`;
+        document.getElementById("results").appendChild(result);
+    
+        if (playerScore == 5 && computerScore < 5) {
+            const finalResult = document.createElement("div");
+            finalResult.textContent = "Player Wins!";
+            document.getElementById("results").appendChild(finalResult);
+        } else if (computerScore == 5 && playerScore < 5) {
+            const finalResult = document.createElement("div");
+            finalResult.textContent = "Computer Wins!";
+            document.getElementById("results").appendChild(finalResult);
+        } else if (playerScore == 5 && computerScore == 5) {
+            const finalResult = document.createElement("div");
+            finalResult.textContent = "Player & Computer Tied!";
+            document.getElementById("results").appendChild(finalResult);
+        }
+    }
+});
+
+const results = document.createElement("div");
+results.setAttribute("id", "results");
+document.body.appendChild(results);
+
+
+
+
+
